b9b7f69f0899b04dba0f1a15c260a80a3e050288
Iryna Hnezdilova <iryna.hnezdilova@gmail.com>
2021-01-13 20:41:48 +0300

    Improvement of the readibility of code

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-let formFind = document.querySelector('.todo-list__create-new-task');
-let ulFind = document.querySelector('.new-task');
-let indexForId = 0;
+let formElement = document.querySelector('.todo-list__create-new-task');
+let ulElement = document.querySelector('.new-task');
+let checkboxCounter = 0;
 
-formFind.addEventListener('submit', function(event) {
+formElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    let formData = new FormData(formFind);  
+    let formData = new FormData(formElement);  
     let toDoName = formData.get('toDo');
 
     if (!toDoName) {
@@ -14,119 +14,61 @@ formFind.addEventListener('submit', function(event) {
     let elToDo = document.createElement('li');
     elToDo.classList.add('new-task__item', 'item');
 
+    //checkbox:
     let btnDone = document.createElement('input');
     btnDone.setAttribute('type', 'checkbox');
+    btnDone.setAttribute('id', 'check' + checkboxCounter); 
     btnDone.classList.add('item__checkbox');
-    btnDone.setAttribute('id', 'check'+indexForId);
-
-    let toDoText = document.createElement('label');
-    toDoText.classList.add('item__checkbox-label');
-    toDoText.setAttribute('for', 'check'+indexForId);
-    indexForId++;
-
-    let btnContainer = document.createElement('div');
-    btnContainer.classList.add('item__buttons-container');
-
-    let checkboxAndTextContainer = document.createElement('div');
-    checkboxAndTextContainer.classList.add('item__checkbox-container');
-
-    let btnDelete = document.createElement('button');
-    //let btnImportant = document.createElement('button');
-
-    btnDelete.classList.add("item__button-delete");
-   // btnImportant.classList.add("item__button-important");
-
-    btnDelete.addEventListener('click', function(event) {
-       event.target.parentNode.parentNode.remove();
-    })
 
     btnDone.addEventListener('click', function(event) {
         toDoText.classList.toggle('crossed');
      })
 
-    /*btnImportant.addEventListener('click', function(event) {
-        elToDo.style.color = 'yellow';
-     })*/
+    
+    //label:
+    let toDoText = document.createElement('label');
+    toDoText.classList.add('item__checkbox-label');
+    toDoText.setAttribute('for', 'check' + checkboxCounter); 
     toDoText.innerText = toDoName;
 
-    checkboxAndTextContainer.appendChild(btnDone);
-    checkboxAndTextContainer.appendChild(toDoText);
-
-    elToDo.appendChild(checkboxAndTextContainer);
-    elToDo.appendChild(btnContainer);
-    
-    //btnContainer.appendChild(btnImportant);
-    btnContainer.appendChild(btnDelete);
-
-    
-    ulFind.appendChild(elToDo);
-
-    formFind.querySelector('.new-task__input').value = '';
-
-    
+    //counter
+    checkboxCounter++;
 
 
+    //containers
+    let btnContainer = document.createElement('div');
+    btnContainer.classList.add('item__buttons-container');
 
-});
+    let checkboxContainer = document.createElement('div');
+    checkboxContainer.classList.add('item__checkbox-container');
 
+    //button delete element
+    let btnDelete = document.createElement('button');
+    btnDelete.classList.add("item__button-delete");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let formFind = document.querySelector('.create_new_todo');
-let ulFind = document.querySelector('.todo');
-
-formFind.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let formData = new FormData(formFind);  
-    let toDoName = formData.get('toDo');
-
-    if (!toDoName) {
-        return;
-    }
-
-    let elToDo = document.createElement('li');
-
-    let btnDone = document.createElement('button');
-    let btnImportant = document.createElement('button');
-
-    btnDone.addEventListener('click', function(event) {
-       event.target.parentNode.remove();
+    btnDelete.addEventListener('click', function(event) {
+       event.target.parentNode.parentNode.remove();
     })
 
-    btnImportant.addEventListener('click', function(event) {
-        elToDo.style.color = 'yellow';
-     })
+    checkboxContainer.appendChild(btnDone);
+    checkboxContainer.appendChild(toDoText);
 
-    elToDo.innerText = toDoName;
-    btnDone.innerText= 'Удалить';
-    btnImportant.innerHTML = 'Важное';
+    elToDo.appendChild(checkboxContainer);
+    elToDo.appendChild(btnContainer);
+    
+    btnContainer.appendChild(btnDelete);
+    
+    ulElement.appendChild(elToDo);
 
-    elToDo.appendChild(btnDone);
-    elToDo.appendChild(btnImportant);
-
-    ulFind.appendChild(elToDo);
-
-    formFind.querySelector('.message').value = '';
+    formElement.querySelector('.new-task__input').value = '';
 });
-*/
+
+
+
+
+
+
+
+
+
+
